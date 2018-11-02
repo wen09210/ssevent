9df971d7ff5d8d55d899c0deb0176cd34c5daf7f
wen09210 <wen09210@gmail.com>
2018-11-02 17:52:12 +0800

    Add files via upload

---
+++ jdata.js
@@ -0,0 +1,7 @@
+var JData = [{ people: "3500", price_font: "3500人",price:"麵包機", img: "img/award/item1.png" },
+             { people: "4500", price_font: "4500人",price:"剉冰機", img: "img/award/item2.png" },
+             { people: "6000", price_font: "6000人",price:"耳機", img: "img/award/item3.png" },
+             { people: "7000", price_font: "7000人",price:"運動攝影機", img: "img/award/item4.png" },
+             { people: "8000", price_font: "8000人",price:"耳機+運動攝影機", img: "img/award/item5.png" },
+             { people: "10000", price_font: "10000人",price:"空氣清淨機", img: "img/award/item6.png" }
+             ]
